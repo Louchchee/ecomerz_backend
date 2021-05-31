8314aab9974987b70476ee56fe0bf2ee9191e04c
Louchchee <louchchee@gmail.com>
2021-05-31 07:27:24 +0530

    serializers' bug fixed

--- base/serializers.py
+++ base/serializers.py
@@ -88,7 +88,7 @@ class OrderSerializer(serializers.ModelSerializer):
     def get_shippingAddress(self, obj):
 
         try:
-            address = ShippingAddressSerializer(obj.shippingaddress, namy=False).data
+            address = ShippingAddressSerializer(obj.shippingaddress, many=False).data
         except:
             address = False    
         return address
